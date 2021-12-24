539d9be13956eaed49794b09e2466ae3b7d0eec5
ajawe <diyar.mohammadi@divar.ir>
2021-12-24 23:54:28 +0330

    Complete ID_Stage_Reg.v

--- ID/ID_Stage_Reg.v
+++ ID/ID_Stage_Reg.v
@@ -1,10 +1,24 @@
 module ID_Stage_Reg(
-  input clk, rst,
-  input[31:0] PC_in,
-  output reg[31:0] PC
-  
+  input clk, rst, flush,
+  input WB_EN_IN, MEM_R_EN_IN, MEM_W_EN_IN, B_IN, S_IN,
+  input[3:0] EXE_CMD_IN,
+  input[31:0] PC_IN,
+  input[31:0] Val_Rn_IN, Val_Rm_IN,
+  input imm_IN,
+  input[11:0] Shift_operand_IN,
+  input[23:0] Signed_imm_24_IN,
+  input[3:0] Dest_IN,
+
+  output WB_EN, MEM_R_EN, MEM_W_EN, B, S,
+  output[3:0] EXE_CMD,
+  output[31:0] PC,
+  output[31:0] Val_Rn, Val_Rm,
+  output imm,
+  output[11:0] Shift_operand,
+  output[23:0] Signed_imm_24,
+  output[3:0] Dest
 );
 
-  always @(posedge clk) PC <= PC_in;
+  always @(posedge clk) PC <= PC_IN;
 
 endmodule
