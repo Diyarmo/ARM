4fafb4e884d0303d9e062aa80b462b0c4cea44ba
ajawe <diyar.mohammadi@divar.ir>
2021-12-26 06:51:07 +0330

    Add StatusRegister.v

---
+++ Others/StatusRegister.v
@@ -0,0 +1,13 @@
+module StatusRegister (
+    input clk, rst,
+    input S,
+    input[3:0] SR_IN,
+    output reg SR
+);
+
+	always@(negedge clk, posedge rst) begin
+		if (rst) SR <= 4'b0;
+		else if (S) SR <= SR_IN;
+	end
+	
+endmodule
